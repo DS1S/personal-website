b7e3c4fa34230b67d6a46639f74e61d43df96290
Daniel <daniel.saunders@mail.utoronto.ca>
2021-12-28 09:09:54 -0500

    project data updates

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -10,7 +10,7 @@ export const projectsData = [
         title: 'Synduce Web Client',
         imgSrc: synduceImg,
         technologies: 'React, AWS Lambda, AWS Gateway, GitHub Actions, Heroku',
-        description: 'Some description',
+        description: 'Synduce is an open-source recursive function transformer. More simply it fills in gaps to incomplete snippets of codes and provides a greater understanding of the algorithm. The web client was built to interface with the CLI app.',
         hasDemo: true,
         demoLink: 'http://synduce.herokuapp.com/',
         githubLink: 'https://github.com/DS1S/synduce-app'
@@ -19,7 +19,7 @@ export const projectsData = [
         title: 'Brackets',
         imgSrc: bracketsImg,
         technologies: 'React, Express, MongoDB, Heroku, GitHub Actions',
-        description: 'Some description',
+        description: 'Brackets is a web-app for tournament organization and attending. Users can create tournaments and manage brackets as well as participate. The project was completed with friends Vedang, Aarya, and Makan.',
         hasDemo: true,
         demoLink: 'https://csc309-brackets.herokuapp.com/',
         githubLink: ''
@@ -28,7 +28,7 @@ export const projectsData = [
         title: 'ArrayIt.js',
         imgSrc: arrayItImg,
         technologies: 'ES6 Javascript, Heroku',
-        description: 'Some description',
+        description: 'ArrayIt.js is a vanilla javascript library used to help those teaching anything related to array algorithms in a simplified manner. Removes worrying about DOM manipulation and focuses more on expressing the algorithm.',
         hasDemo: true,
         demoLink: 'https://pure-fortress-27269.herokuapp.com/',
         githubLink: ''
@@ -37,7 +37,7 @@ export const projectsData = [
         title: 'Covid API',
         imgSrc: covidTrackerImg,
         technologies: 'Python, FastAPI, GitHub Actions, Heroku',
-        description: 'Some description',
+        description: 'Covid API stores covid data uploaded and upon request can be used to synthesize the raw csv files previously provided. Utilizes design patterns and clean architecture to establish a robust API.',
         hasDemo: true,
         demoLink: 'https://a2-covid-data-api.herokuapp.com/',
         githubLink: ''
@@ -46,7 +46,7 @@ export const projectsData = [
         title: 'Tech Conference',
         imgSrc: techConferenceImg,
         technologies: 'Java',
-        description: 'Some description',
+        description: 'Tech Conference is a java application meant to demonstrate the benfit of clean architecture and SOLID principles. Built with a team of 8, took on lead-design role and programming of the majority of systems.',
         hasDemo: false,
         githubLink: 'https://github.com/DS1S/techconference-app'
     }
